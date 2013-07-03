f1c7cf0cf4243f3da2225bfb80536413a0a9ce25
Marco Tizzoni <elibus@gmail.com>
2013-07-03 20:08:01 +0200

    CheckStyle fixes

--- gtrends-api/src/main/java/org/freaknet/gtrends/api/gtrendsapi/GoogleTrendsClient.java
+++ gtrends-api/src/main/java/org/freaknet/gtrends/api/gtrendsapi/GoogleTrendsClient.java
@@ -1,8 +1,6 @@
 package org.freaknet.gtrends.api.gtrendsapi;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.http.HttpResponse;
@@ -41,10 +39,10 @@ public class GoogleTrendsClient {
             HttpRequestBase httpRequest = request.build();
             HttpResponse response = client.execute(httpRequest);
             ret = GoogleUtils.toString(response.getEntity().getContent());
-            
+
             HttpGet get = new HttpGet("http://www.google.com");
             get.addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21");
-            get.addHeader("Accept","text/plain");
+            get.addHeader("Accept", "text/plain");
             get.addHeader("Referrer", "https://www.google.com/accounts/ServiceLoginBoxAuth");
             client.execute(get);
 
